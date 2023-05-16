08bce92e8f2d8c659cd29a102a54cb409694865d
Yujun Han <48095063+atomic0x90@users.noreply.github.com>
2023-05-16 11:34:41 +0900

    Add line_chart

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x*x)
+
+df = pd.DataFrame(np.random.randn(10,2), columns=['x','y'])
+st.line_chart(df)
